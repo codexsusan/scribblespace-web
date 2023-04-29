a70a12814ae488256ff0215628c29b1f989462a3
Susan Khadka <susankhadka708@gmail.com>
2023-04-29 10:54:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      Hello
+    </div>
+  )
+}
+
+export default App
